b7707c52eecb445602860ae3d30ab9ddd01677cf
Alan-G05 <dealanfoxi@gmail.com>
2025-12-16 17:17:36 -0600

    16-12-25 12:17 Cambios en itineario - index, controller y model

--- src/presentation/itinerario/itinerario.model.ts
+++ src/presentation/itinerario/itinerario.model.ts
@@ -1,40 +1,51 @@
+//CAMBIOS DE HAROL
 import { t } from "elysia";
 
 export namespace ItinerarioModel {
-    
-    export const getItinerarioParams = t.Object({
-        id: t.String()
-    });
+  export const getItinerarioParams = t.Object({
+    id: t.String(),
+  });
 
-    export const regItinerarioCuerpo = t.Object({
-        title: t.String({ error: "Debe llevar un título el itinerario" }),
-        actividades: t.Optional(
-            t.Array(
-                t.Object({
-                    fecha: t.Optional(t.String({ error: "Debe llevar una hora de inicio" })),
-                    description: t.String({ error: "Debe llevar una descripción" }),
-                    lugarId: t.String({ error: "Debe llevar un lugar" })
-                })
-            )
-        )
-    });
+  export const regItinerarioCuerpo = t.Object({
+    title: t.String({ error: "Debe llevar un título el itinerario" }),
+    actividades: t.Optional(
+      t.Array(
+        t.Object({
+          fecha: t.Optional(
+            t.String({ error: "Debe llevar una hora de inicio" })
+          ),
+          description: t.String({ error: "Debe llevar una descripción" }),
+          lugarId: t.String({ error: "Debe llevar un lugar" }),
+        })
+      )
+    ),
+  });
 
-    export type RegItinerarioCuerpo = typeof regItinerarioCuerpo.static;
+  export type RegItinerarioCuerpo = typeof regItinerarioCuerpo.static;
 
-    export const modItinerarioCuerpo = t.Object( //Para modificar el itinerario
-        {
-            title: t.Optional(t.String()),
-        }
-    )
+  export const modItinerarioCuerpo = t.Object({
+    title: t.String({ error: "Debe llevar un título el itinerario" }),
+    actividades: t.Optional(
+      t.Array(
+        t.Object({
+          fecha: t.Optional(
+            t.String({ error: "Debe llevar una hora de inicio" })
+          ),
+          description: t.String({ error: "Debe llevar una descripción" }),
+          lugarId: t.String({ error: "Debe llevar un lugar" }),
+        })
+      )
+    ),
+  });
 
-    export type ModItinerarioCuerpo = typeof modItinerarioCuerpo.static;
+  export type ModItinerarioCuerpo = typeof modItinerarioCuerpo.static;
 
-    /// buscar itinerarios 
-    export const buscarIti = t.Object({
-        q: t.Optional(t.String()),        // Término de búsqueda general
-        category: t.Optional(t.String()), // Nuevo filtro
-        state: t.Optional(t.String())     // Nuevo filtro
-    })
+  /// buscar itinerarios
+  export const buscarIti = t.Object({
+    q: t.Optional(t.String()), // Término de búsqueda general
+    category: t.Optional(t.String()), // Nuevo filtro
+    state: t.Optional(t.String()), // Nuevo filtro
+  });
 
-    export type BuscarIti = typeof buscarIti.static
+  export type BuscarIti = typeof buscarIti.static;
 }
